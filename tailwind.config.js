87a7b36d13c75185a9933918b6ce6dfd12539cf6
pavlogreat <mrskilder@gmail.com>
2025-02-11 02:22:57 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+import { type Config } from "tailwindcss"
+
+export default {
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "sm": "640px",
+        "md": "768px",
+        "lg": "1024px",
+        "xl": "1280px",
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "#0057B7",
+          foreground: "#FFFFFF",
+        },
+        secondary: {
+          DEFAULT: "#FFDD00",
+          foreground: "#000000",
+        },
+      },
+      fontFamily: {
+        tektur: ['Tektur', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config
+
